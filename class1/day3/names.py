05675040d4d6ee247365e3f5f5cff10d36cbd341
L0oop1 <L0oop1@hotmail.com>
2026-06-05 22:31:29 -0700

    Add names.py to greet user and introduce a name

---
+++ class1/day3/names.py
@@ -0,0 +1,11 @@
+# Asking for your name
+name = input("What is your name: ")
+my_name = input("Think of a name: ")
+
+# thanking your name
+print(name," is an awesome name!")
+
+# Asking you my name
+print("I'm", my_name,", I like trains.")
+
+print("---L0oop1---")
