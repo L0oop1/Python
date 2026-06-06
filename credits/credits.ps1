bd411fe7f328c4f24db8803cab682485a03322e0
L0oop1 <L0oop1@hotmail.com>
2026-06-05 22:58:11 -0700

    Add credits.ps1 with initial content

---
+++ credits/credits.ps1
@@ -0,0 +1,4 @@
+Write-Host "L0oop1"
+Write-Host "That's it so far, but you can make a pull request to add your self here!"
+write-Host "But you have to add/edit stuff, though"
+Write-Host "---L0oop1---"
